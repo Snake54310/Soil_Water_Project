c038b815f61cf407879ce24d2203fa27b9bfd379
Snake54310 <86499062+Snake54310@users.noreply.github.com>
2026-03-17 01:13:51 -0600

    temperature sensor template added

---
+++ Sensor_templates/HTS221_AirTraits.py
@@ -0,0 +1,34 @@
+'''
+import time
+import board
+import busio
+import adafruit_hts221
+
+i2c = busio.I2C(board.SCL, board.SDA)
+hts = adafruit_hts221.HTS221(i2c)
+
+print("Relative Humidity: %.2f %% rH" % hts.relative_humidity)
+print("Temperature: %.2f C" % hts.temperature)'''
+
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+
+import board
+
+import adafruit_hts221
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+hts = adafruit_hts221.HTS221(i2c)
+
+data_rate = adafruit_hts221.Rate.label[hts.data_rate]
+print(f"Using data rate of: {data_rate:.1f} Hz")
+print("")
+
+while True:
+    print(f"Relative Humidity: {hts.relative_humidity:.2f} % rH")
+    print(f"Temperature: {hts.temperature:.2f} C")
+    print("")
+    time.sleep(1)
