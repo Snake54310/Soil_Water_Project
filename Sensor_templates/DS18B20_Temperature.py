7a96cdf312cf333feeb46a304a8a17bcb1b2e9c0
Snake54310 <86499062+Snake54310@users.noreply.github.com>
2026-03-18 15:03:32 -0600

    Updated code for ground temp sensor

--- Sensor_templates/DS18B20_Temperature.py
+++ Sensor_templates/DS18B20_Temperature.py
@@ -1,5 +1,9 @@
 from w1thermsensor import W1ThermSensor
 
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 sensor = W1ThermSensor()           # auto-detects the first sensor
 temperature_in_c = sensor.get_temperature()
 # or: temperature_in_f = sensor.get_temperature(W1ThermSensor.DEGREES_F)
